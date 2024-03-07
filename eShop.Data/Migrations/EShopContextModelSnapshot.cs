0adf1948d8ece42abc08a68d4e30807f1fabbe77
Eddie Wahlström <eddie.wahlstrom@edu.newton.se>
2024-03-07 09:54:59 +0100

    Changed AsideComponent to Asidecomponent in Home.razor

--- eShop.Data/Migrations/EShopContextModelSnapshot.cs
+++ eShop.Data/Migrations/EShopContextModelSnapshot.cs
@@ -17,7 +17,7 @@ namespace eShop.Data.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "8.0.2")
+                .HasAnnotation("ProductVersion", "8.0.1")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
