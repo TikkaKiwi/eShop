2ffc3bab0b1b510c9da666926af3d353978076f8
Martin Olsson <martinolsson89@gmail.com>
2024-03-12 16:33:49 +0100

    added truncate to Description

---
+++ eShop.UI.Admin/Extensions/StringExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace eShop.UI.Admin.Extensions;
+
+public static class StringExtensions
+{
+    public static string Truncate(this string value, int length)
+    {
+        if(value.Length <= length)
+            return value;
+
+        return value.Trim().Substring(0, length - 3).Trim() + "...";
+    }
+}
